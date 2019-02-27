9e0fb34d38832b9e5d6b33e4cc73de3d46aec5c4
sheldon <dragon1992long@163.com>
2019-02-27 16:29:08 +0800

    继承AppCompatActivity，修复权限弹窗crash问题；

--- app/src/main/java/com/lxl/easy/mqtt/demo/MainActivity.java
+++ app/src/main/java/com/lxl/easy/mqtt/demo/MainActivity.java
@@ -15,6 +15,7 @@ import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
@@ -32,7 +33,7 @@ import com.lxl.easy.mqtt.IEasyMqttCallBack;
 /**
  * @author sheldon
  */
-public class MainActivity extends Activity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private String TAG="EasyMQTT";
     private EasyMqttService mqttService;
@@ -105,99 +106,10 @@ public class MainActivity extends Activity implements View.OnClickListener {
         close(); //关闭MQTT连接
     }
 
-    /**
-     * 初始化UI变量
-     */
-    @SuppressLint("HardwareIds")
-    public void setupView()
-    {
-        // lock the screen in portrait mode
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-
-        sDeviceID = Secure.getString(this.getContentResolver(), Secure.ANDROID_ID);
-        addressET = (EditText)findViewById(R.id.addressEditText);
-        String serverAddr = Utils.getPreferences(getApplicationContext(),"address");
-        addressET.setText(serverAddr==null?"tcp://47.98.206.54:8083":serverAddr);
-        userNameET = (EditText)findViewById(R.id.userNameEditText);
-        userNameET.setText(Utils.getPreferences(getApplicationContext(),"userName"));
-        passwordET = (EditText)findViewById(R.id.passwordEditText);
-        topicET = (EditText)findViewById(R.id.topicEditText);
-        topicET.setText(Utils.getPreferences(getApplicationContext(),"topic"));
-        messageET = (EditText)findViewById(R.id.messageEditText);
-        messageET.setText(Utils.getPreferences(getApplicationContext(),"message"));
-        receiveET = (EditText)findViewById(R.id.receiveEditText);
-
-        connectButton = (Button)findViewById(R.id.connectButton);
-        connectButton.setOnClickListener(this);
-
-        disconnectButton = (Button)findViewById(R.id.disconnectButton);
-        disconnectButton.setOnClickListener(this);
-
-        sendButton = (Button)findViewById(R.id.sendButton);
-        sendButton.setOnClickListener(this);
-    }
 
     /**
-     * 检查是否拥有指定的所有权限
+     * 按键监听
      */
-    private boolean checkPermissionAllGranted(String[] permissions) {
-        for (String permission : permissions) {
-            if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
-                // 只要有一个权限没有被授予, 则直接返回 false
-                return false;
-            }
-        }
-        return true;
-    }
-
-    /**
-     * 打开 APP 的详情设置
-     */
-    private void openAppDetails() {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage(getResources().getString(R.string.permission_guide));
-        builder.setPositiveButton(getResources().getString(R.string.permission_manual), new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                Intent intent = new Intent();
-                intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
-                intent.addCategory(Intent.CATEGORY_DEFAULT);
-                intent.setData(Uri.parse("package:" + getPackageName()));
-                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
-                intent.addFlags(Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
-                startActivity(intent);
-            }
-        });
-        builder.setNegativeButton(getResources().getString(R.string.permission_cancel), null);
-        builder.show();
-    }
-
-    /**
-     * 申请权限结果返回处理
-     */
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-
-        if (requestCode == MQTT_PERMISSION_REQUEST_CODE) {
-            boolean isAllGranted = true;
-
-            // 判断是否所有的权限都已经授予了
-            for (int grant : grantResults) {
-                if (grant != PackageManager.PERMISSION_GRANTED) {
-                    isAllGranted = false;
-                    break;
-                }
-            }
-
-            if (!isAllGranted) {
-                // 弹出对话框告诉用户需要权限的原因, 并引导用户去应用权限管理中手动打开权限按钮
-                openAppDetails();
-            }
-        }
-    }
-
     @Override
     public void onClick(View v) {
         if(v == connectButton)
@@ -244,6 +156,99 @@ public class MainActivity extends Activity implements View.OnClickListener {
                 send();
             }
         }
+    }
+
+    /**
+     * 申请权限结果返回处理
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == MQTT_PERMISSION_REQUEST_CODE) {
+            boolean isAllGranted = true;
+
+            // 判断是否所有的权限都已经授予了
+            for (int grant : grantResults) {
+                if (grant != PackageManager.PERMISSION_GRANTED) {
+                    isAllGranted = false;
+                    break;
+                }
+            }
+
+            if (!isAllGranted) {
+                // 弹出对话框告诉用户需要权限的原因, 并引导用户去应用权限管理中手动打开权限按钮
+                openAppDetails();
+            }
+        }
+    }
+
+    /**
+     * 检查是否拥有指定的所有权限
+     */
+    private boolean checkPermissionAllGranted(String[] permissions) {
+        for (String permission : permissions) {
+            if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+                // 只要有一个权限没有被授予, 则直接返回 false
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * 打开 APP 的详情设置
+     */
+    private void openAppDetails() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(getResources().getString(R.string.permission_guide));
+        builder.setPositiveButton(getResources().getString(R.string.permission_manual), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Intent intent = new Intent();
+                intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                intent.addCategory(Intent.CATEGORY_DEFAULT);
+                intent.setData(Uri.parse("package:" + getPackageName()));
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+                intent.addFlags(Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
+                startActivity(intent);
+            }
+        });
+        builder.setNegativeButton(getResources().getString(R.string.permission_cancel), null);
+        builder.show();
+    }
+
+    /**
+     * 初始化UI变量
+     */
+    @SuppressLint("HardwareIds")
+    public void setupView()
+    {
+        // lock the screen in portrait mode
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        sDeviceID = Secure.getString(this.getContentResolver(), Secure.ANDROID_ID);
+        addressET = (EditText)findViewById(R.id.addressEditText);
+        String serverAddr = Utils.getPreferences(getApplicationContext(),"address");
+        addressET.setText(serverAddr==null?"tcp://47.98.206.54:8083":serverAddr);
+        userNameET = (EditText)findViewById(R.id.userNameEditText);
+        userNameET.setText(Utils.getPreferences(getApplicationContext(),"userName"));
+        passwordET = (EditText)findViewById(R.id.passwordEditText);
+        topicET = (EditText)findViewById(R.id.topicEditText);
+        topicET.setText(Utils.getPreferences(getApplicationContext(),"topic"));
+        messageET = (EditText)findViewById(R.id.messageEditText);
+        messageET.setText(Utils.getPreferences(getApplicationContext(),"message"));
+        receiveET = (EditText)findViewById(R.id.receiveEditText);
+
+        connectButton = (Button)findViewById(R.id.connectButton);
+        connectButton.setOnClickListener(this);
+
+        disconnectButton = (Button)findViewById(R.id.disconnectButton);
+        disconnectButton.setOnClickListener(this);
+
+        sendButton = (Button)findViewById(R.id.sendButton);
+        sendButton.setOnClickListener(this);
     }
 
     /**
